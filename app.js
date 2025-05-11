d9b1811f78f36310382bab27ead2a8fb4b8fe3f0
Mmatswene Malete <601302@student.belgiumcampus.ac.za>
2025-05-11 15:53:48 +0200

    installing express, fixing the routes because the folder path was wrong, configuring the GET routes, implementing the POST route for the contact form and thankyou redirect

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 // app.js
 
 const express = require("express");
+const bodyParser = require('body-parser')
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views", "pages"));
 
 app.use("/", pageRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,18 +4,39 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    //render the "home.ejs" view
+    res.render('home')
 });
 
 router.get('/about', (req, res) => {
+     //render the "about.ejs" view
+    res.render('about')
 });
 
 router.get('/events', (req, res) => {
+     //render the "events.ejs" view
+    res.render('events')
 });
 
 router.get('/contact', (req, res) => {
+     //render the "contact.ejs" view
+    res.render('contact')
 });
 
 router.get('/thankyou', (req, res) => {
+     //render the "thankyou.ejs" view after file submission
+    res.render('thankyou')
 });
 
+//contact form submission route (POST/contact)
+
+router.post('/contact', (req,res) => {
+    const { name, email, message } = req.body;
+    //Print the form data to the console
+    console.log('Contact form submitted: '+ {name, email, message});
+
+    //Redirect to the "thankyou" page after form submission
+    res.redirect('/thankyou');
+});
+ 
 module.exports = router;
