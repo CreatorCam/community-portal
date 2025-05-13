9505fbb365d24602abe4751fb537f9184bd52691
Bahle Sithole <masithole.simphiwe@gmail.com>
2025-05-13 13:10:53 +0200

    Added some code

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const pageRoutes = require("./routes/pageRoutes");
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// Set EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/pages'));
+
+// Routes
+app.use('/', pageRoutes);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
